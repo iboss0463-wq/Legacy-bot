bec31cd1f8e0b79c488934cf7074c67971d75968
iboss0463-wq <iboss0463@gmail.com>
2026-04-29 11:39:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is Online!'));
+app.listen(3000, () => console.log('Server is ready!'));
 const { 
     Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, 
     ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, 
